f932fa956537c2eff4909704248f761c69486879
deepakcr7 <deepakcr7@users.noreply.github.com>
2021-08-28 18:33:40 +0530

    Update ec2.tf

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2_instance" {
   ami           = "${var.ami}"
   instance_type = "${var.instance_type}"
-  vpc_id = "${var.vpc_id}"
+  subnet_id = "${var.subnet_id}"
   tags = {
     Name = "${var.name_tag}"
   }
