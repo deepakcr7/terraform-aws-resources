b4fe8cca95e583c32c3df0a8bb1818e0de67be9b
deepakcr7 <deepakcr7@users.noreply.github.com>
2021-08-28 18:28:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,5 +4,5 @@ count = length(var.instance_type)
 ami = var.amis[count.index]
 instance_type = var.instance_type[count.index]
 name_tag = var.name_tag[count.index]
-vpc_id = "vpc-07066e70f2a716d7a"
+vpc_id = var.vpc_id
 }
